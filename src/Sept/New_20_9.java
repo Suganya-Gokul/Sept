08a5a2d495b174ce5270ceb162cced2b5498ce21
Suganya-Gokul <suganya.vidhu97@gmail.com>
2024-09-20 16:50:00 +0530

    Sept-20

---
+++ src/Sept/New_20_9.java
@@ -0,0 +1,24 @@
+package Sept;
+
+public class New_20_9 {
+    public static void main(String[] args) {
+        //int a=10, b=20, c=45;
+        //condition ? expression_if_true: expression_if_false
+
+       /* int max_a = (a>b)? a:b;
+        System.out.println(max_a);
+
+        int max = (max_a>c)?max_a:c;
+        System.out.println(max);*/
+
+        //Ques2:
+        //  score >= 90 → A
+        //  score >= 80 → B
+        //  score >= 70 → C
+
+        int score=85;
+        String result = (score>=90) ? "A":(score>=80)? "B":(score>=70)?"C":"F";
+        System.out.println(result);
+
+    }
+}
