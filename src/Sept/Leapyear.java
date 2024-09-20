aef1e0e25333943b88123888e340e5440d0ec6c2
Suganya-Gokul <suganya.vidhu97@gmail.com>
2024-09-20 16:50:00 +0530

    Sept-20

---
+++ src/Sept/Leapyear.java
@@ -0,0 +1,29 @@
+package Sept;
+
+public class Leapyear {
+    public static void main(String[] args) {
+        int year = 2024;
+        if (year %4==0)
+        {
+            if (year%100==0)
+            {
+                if (year %400 == 0)
+                {
+                    System.out.println("It is leap year");
+                }
+                else
+                {
+                    System.out.println("Not a leap year");
+                }
+            }
+            else
+            {
+                System.out.println("Not a leap year");
+            }
+            }
+        else
+        {
+            System.out.println("It is a leap year");
+        }
+    }
+}
